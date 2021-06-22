66716bd9ebf60286cd841286aad11bc3b7dea33f
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-22 13:11:06 +0200

    Add files via upload

---
+++ Guess the name/script.js
@@ -0,0 +1,10 @@
+var username = prompt("Welkom! Wat is je naam?")
+alert("Hey " + username)
+
+Math.random(number =< 25);
+var chooseNumber = prompt(username + "Voer een nummer in van 0 tot 25 om te beginnen met raden...") {
+    if (number = math.random) {
+        alert(messag)
+    }
+}
+
